644e0b183dfeb4309aa3ab1994b4f92749dd49c7
eratliffcrain <eratliffcrain@gmail.com>
2022-11-28 09:43:15 -0500

    Adding argument parsing for run_model. Bug fixes

--- run_model.py
+++ run_model.py
@@ -18,6 +18,7 @@ code to run the facility placement abm
 import numpy as np 
 import pandas as pd 
 import geopandas as gpd
+import argparse
 
 from shapely.errors import ShapelyDeprecationWarning
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
@@ -30,12 +31,39 @@ from copy import deepcopy
 
 from facility_ABM import *
 from multiprocessing import Pool,freeze_support
+from pathlib import Path
+
+n_facilities = 1070
 
 
+def get_parser():
+    parser = argparse.ArgumentParser(
+        usage=f"python {__name__}.py"    
+    )
+    parser.add_argument(
+        "--n_facilities",
+        type=int,
+        default=1070
+    )
+    parser.add_argument(
+        "--num_steps",
+        type=int,
+        default=2
+    )
+    parser.add_argument(
+        "--legal_states_only",
+        #type=bool,
+        action=argparse.BooleanOptionalAction,
+    )
+    return parser
 
 
-
-def main():
+def main(
+    n_facilities: int = 1070,
+    num_steps: int = 2,
+    legal_states_only: bool = True,
+    out_path: Path = Path("./"),
+):
     """
     GLOBAL VARIABLES
     """
@@ -51,28 +79,25 @@ def main():
     gdf_pop = gpd.GeoDataFrame(df_pop,geometry = gpd.points_from_xy(df_pop.lon,df_pop.lat)).rename(columns = {"Unnamed: 0":"index"})#.rename(columns ={" Unnamed: 0",'index'})#generate initial facility placement
     #load state boundaries
     df_state = pop.load_state_bounds(COUNTY_SHP_FILE)
-    df_state_legal = df_state[~df_state['STATE_NAME'].isin(BANNED_STATES)]
+    if legal_states_only:
+        df_state_legal = df_state[~df_state['STATE_NAME'].isin(BANNED_STATES)]
+    else:
+        df_state_legal = df_state
     #pull out polygon
-    us_border = df_state.dissolve().geometry.values[0]#extract shapley polygons from dataframe
+    # us_border = df_state.dissolve().geometry.values[0]#extract shapley polygons from dataframe
     
     #cut down to subset of states with legalized facilites
-    
-    
-    us_legal_border = df_state.dissolve().geometry.values[0]#extract shapley polygon from dataframe
-    fac_placements_df = pop.random_points_within_polygon(us_border,100).reset_index()#
+    us_legal_border = df_state_legal.dissolve().geometry.values[0]#extract shapley polygon from dataframe
+    fac_placements_df = pop.random_points_within_polygon(us_legal_border,n_facilities).reset_index()#
     
     #calculate the distance to the nearest facility
     fac_pop_dist_df = pop.calc_facility_distance(gdf_pop,fac_placements_df)
     objective_function_val = pop.objective_function(fac_pop_dist_df,'distance','nearest_fac')
     
-    
-    
-    num_steps = 1
-    
-    for i in range(num_steps):#for each timestep    
+    for i in range(num_steps):#for each timestep  
         print("step: {}".format(i))
         #generate a test facility placement by moving an agent
-        test_fac_placements_df = move_agents(fac_placements_df,us_border,1)
+        test_fac_placements_df = move_agents(fac_placements_df,us_legal_border,1)
         #calculate the distances for this new facility placement
         test_fac_pop_dist_df = calc_facility_distance(gdf_pop,test_fac_placements_df)
         #calculate the objective function for this new facility placement 
@@ -90,13 +115,18 @@ def main():
                 fac_placements_df = test_fac_placements_df#use the new facility placement instead of the old one
                 print("Replacing old facility list with score {} with new facility list with score {}".format(objective_function_val,test_objective_function_val))
     
+    if legal_states_only:
+        states = "legal"
+    else:
+        states = "all"
+    out_path = out_path / f"{states}_{num_steps}steps_placement.parq"
+    fac_placements_df.to_parquet(out_path)
     
-
 
 if __name__=="__main__":
     __spec__ = None
     freeze_support()
-    main()
+    main(**vars(get_parser().parse_args()))
 
 
 
