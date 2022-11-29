479e21207fcdd0e0df8a29978c9da088e01f4ba7
WillHWThompson <will.w.thompson@gmail.com>
2022-11-28 19:44:29 -0500

    refactor KNN

--- facility_ABM.py
+++ facility_ABM.py
@@ -14,7 +14,8 @@ from shapely.ops import nearest_points
 from copy import deepcopy
 from multiprocessing import Pool,freeze_support
 from functools import partial
-
+import swifter
+from sklearn.neighbors import BallTree, KDTree
 
 
 """
@@ -62,9 +63,8 @@ random_points_within_polygon(); raturns a list of n points within a given polygo
    Output:
         points<list{Shapley.geometry.Points}: a list of points randomly distributed within the polygon
 """
-def random_points_within_polygon(polygon,number):
+def random_points_within_polygon(polygon,number,EPSG=4326):
     points = []
-
     #generate points within the polygons bounding box
     minx,miny,maxx,maxy = polygon.bounds
     while len(points) <  number:
@@ -72,7 +72,8 @@ def random_points_within_polygon(polygon,number):
         #check if points fall within a polygon
         if polygon.contains(pnt):
             points.append(pnt)
-    return gpd.GeoDataFrame(geometry=points)
+    gdf =  gpd.GeoDataFrame(geometry=points).set_crs(EPSG)
+    return gdf
 
 """
 nearest():calculate the nearest neighbor between two rows of dataframe
@@ -88,6 +89,51 @@ def nearest(row,df2,geom1_col = 'geometry'):
 def get_distance(row,geom1_col,geom2_col):
     return row[geom1_col].distance(row[geom2_col])
 
+"""
+add a lot and lon column to a GeoDataFrame with a geometry row with Shapley Point objects
+input: 
+    df<GeoDataFrame>: a datafarme with a column with Point objects
+output:<numpy array> the x and y coordinates of those points
+"""
+def get_coords_from_shape(df,geom_row="geometry"):
+    return np.stack(df[geom_row].apply(lambda coord:[coord.x,coord.y]).tolist(),axis = 0)
+
+# """
+# calc_facility_distance(): takes in a df of population and facility placements, returns a df the nearest facility to each individual and the distance
+# inpukt:
+#         df1<GeoDataFrame>: a datafame with Shapley.geometry.Point objects for each individual in the population
+#         df2<GeoDataFrame>: a datafame with Shapley.geometry.Point objects for each facility
+#         geom1_col<str>: the column in <df1> that contains the point objects
+#         geom2_col<str>: the column in <df2> that contains the point objects
+# returns: 
+#         concat_df<GeoDataFrame>: a df with the position of each indiviudal, its nearest facility and the distance
+# """
+# def calc_facility_distance(df1,df2,geom1_col='geometry',geom2_col="geometry"):
+#     #calculate the nearest facilty to each member of the population
+     
+#     # start_time = time.time()
+#     # nearest_fac_series = df1.apply(nearest,df2=df2,geom1_col=geom1_col,axis = 1)
+#     # time1 = time.time()-start_time
+    
+#     start_time = time.time()
+#     nearest_fac_series = paralellize_on_rows(df1,nearest,df2=df2,geom1_col=geom1_col)
+#     time2 = time.time()-start_time
+
+#     start_time = time.time()
+#     nearest_fac_series = df1.swifter.apply(nearest,df2=df2,geom1_col=geom1_col,axis = 1)
+#     time3 = time.time()-start_time
+
+#     print("time1:{},time2:{},time3:{}".format(time1,time2,time3))
+
+#     #reformat nearest facility data into a df
+#     nearest_fac_df = nearest_fac_series.reset_index().rename(columns = {0:"nearest_fac"}).set_geometry("nearest_fac")
+#     #calculate the distance between each individual and the nearest facility
+#     distance_df=df1['geometry'].distance(nearest_fac_df).reset_index().rename(columns = {0:"distance"})
+#     #join all data and return
+#     concat_df = pd.concat([df1,nearest_fac_df,distance_df],axis = 1).drop("index",axis = 1)
+#     return concat_df
+
+
 
 """
 calc_facility_distance(): takes in a df of population and facility placements, returns a df the nearest facility to each individual and the distance
@@ -99,33 +145,38 @@ inpukt:
 returns: 
         concat_df<GeoDataFrame>: a df with the position of each indiviudal, its nearest facility and the distance
 """
-def calc_facility_distance(df1,df2,geom1_col='geometry',geom2_col="geometry"):
-    #calculate the nearest facilty to each member of the population
+
+def calc_facility_distance(df1,df2,DISTANCE_EPSG = 3857):
+
+    #convert coordinates to a flat projection for mercator
+    df1 = df1.to_crs(DISTANCE_EPSG)
+    df1 = df2.to_crs(DISTANCE_EPSG)
      
-    #start_time = time.time()
-    #nearest_fac_series = df1.apply(nearest,df2=df2,geom1_col=geom1_col,axis = 1)
-    #time1 = time.time()-start_time
-    
-    start_time = time.time()
-    nearest_fac_series = paralellize_on_rows(df1,nearest,df2=df2,geom1_col=geom1_col)
-    time2 = time.time()-start_time
+    lat_lon_arr = np.stack(get_coords_from_shape(df2),axis = 0)#generate lat long pairs from Point objects
+    x= lat_lon_arr[:,0]
+    y = lat_lon_arr[:,1]
+    tree = KDTree(np.c_[x,y])#create search tree
+    #get population coordinates
+    pop_coords = np.stack(get_coords_from_shape(df1),axis=0)
 
-    print("time1:{},time2:{}".format(time1,time2))
+    dd,ii = tree.query(pop_coords,k=1)#find the nearest facilty 
 
-    #reformat nearest facility data into a df
-    nearest_fac_df = nearest_fac_series.reset_index().rename(columns = {0:"nearest_fac"}).set_geometry("nearest_fac")
-    #calculate the distance between each individual and the nearest facility
-    distance_df=df1['geometry'].distance(nearest_fac_df).reset_index().rename(columns = {0:"distance"})
-    #join all data and return
-    concat_df = pd.concat([df1,nearest_fac_df,distance_df],axis = 1).drop("index",axis = 1)
-    return concat_df
+    #merged nerest neighbor info with facility info
+    df1['distance'] = dd
+    df1['nearest_fac'] = ii
+    df_fac_pop_merged = df1.merge(df2,left_on = 'nearest_fac',right_on = "index",suffixes=('_pop','_fac'))
+    df_fac_pop_merged.drop(['index_pop','index_fac'],axis =1,inplace=True)
+    df_fac_pop_merged.rename(columns={'geometry_pop':'geometry','geometry_fac':'nearest_fac'})
+    return df_fac_pop_merged
+
 
 """
 calculate the objective function(dist^beta) for each facility. return the result as a dataframe
 """
 def objective_function(df,value_col,groupby_col,beta=1):
     df['weighted_dist'] = df[value_col]**beta#take the distance to an exponential
-    df = df.groupby(df[groupby_col].to_wkt()).agg(total_fac_weighted_dist = ('weighted_dist',sum))
+    #df = df.groupby(df[groupby_col].to_wkt()).agg(total_fac_weighted_dist = ('weighted_dist',sum))
+    df = df.groupby(df[groupby_col]).agg(total_fac_weighted_dist = ('weighted_dist',sum))
     return df['total_fac_weighted_dist'].sum()
 
 """
--- run_model.py
+++ run_model.py
@@ -3,7 +3,7 @@ def warn(*args, **kwargs):
 import warnings
 warnings.warn = warn
 
-import gen_population as pop
+#import gen_population as pop
 import facility_ABM
 import pandas as pd
 import geopandas as gpd
@@ -34,6 +34,7 @@ from multiprocessing import Pool,freeze_support
 from pathlib import Path
 
 n_facilities = 1070
+EPSG_LON_LAT = 4326 # the EPSG which represents pairs of lon lat points
 
 
 def get_parser():
@@ -55,14 +56,21 @@ def get_parser():
         #type=bool,
         action=argparse.BooleanOptionalAction,
     )
+
+    parser.add_argument(
+        "--pop_size",
+        type=int,
+        default=-1#if set to -1, the full population will be used
+    )
     return parser
 
 
 def main(
     n_facilities: int = 1070,
-    num_steps: int = 2,
+    num_steps: int = 4,
     legal_states_only: bool = True,
     out_path: Path = Path("./"),
+    pop_size: int = -1 
 ):
     """
     GLOBAL VARIABLES
@@ -74,11 +82,21 @@ def main(
     """"
     init the model for a single facility
     """
-    #load county dat    a 
+    print("loading pop point data...")
     df_pop = pd.read_csv("data/simulated_pop_points.csv")
-    gdf_pop = gpd.GeoDataFrame(df_pop,geometry = gpd.points_from_xy(df_pop.lon,df_pop.lat)).rename(columns = {"Unnamed: 0":"index"})#.rename(columns ={" Unnamed: 0",'index'})#generate initial facility placement
+    gdf_pop = gpd.GeoDataFrame(df_pop,geometry = gpd.points_from_xy(df_pop.lon,df_pop.lat)).rename(columns = {"Unnamed: 0":"index"}).set_crs(EPSG_LON_LAT)#.rename(columns ={" Unnamed: 0",'index'})#generate initial facility placement
+    print(f"population size: {gdf_pop.size}")
+    full_pop_size = gdf_pop.shape[0]
+
+    #load population size
+    if (pop_size == -1) or (pop_size > full_pop_size):
+        print("setting population size to full:{}".format(full_pop_size))
+        pop_size = full_pop_size
+    gdf_pop = gdf_pop.sample(pop_size)#cut the sample size down
+    print("loaded population points containing {} points".format(pop_size))
+
     #load state boundaries
-    df_state = pop.load_state_bounds(COUNTY_SHP_FILE)
+    df_state = load_state_bounds(COUNTY_SHP_FILE)
     if legal_states_only:
         df_state_legal = df_state[~df_state['STATE_NAME'].isin(BANNED_STATES)]
     else:
@@ -88,11 +106,11 @@ def main(
     
     #cut down to subset of states with legalized facilites
     us_legal_border = df_state_legal.dissolve().geometry.values[0]#extract shapley polygon from dataframe
-    fac_placements_df = pop.random_points_within_polygon(us_legal_border,n_facilities).reset_index()#
+    fac_placements_df = random_points_within_polygon(us_legal_border,n_facilities).reset_index()#
     
     #calculate the distance to the nearest facility
-    fac_pop_dist_df = pop.calc_facility_distance(gdf_pop,fac_placements_df)
-    objective_function_val = pop.objective_function(fac_pop_dist_df,'distance','nearest_fac')
+    fac_pop_dist_df = calc_facility_distance(gdf_pop,fac_placements_df)
+    objective_function_val = objective_function(fac_pop_dist_df,'distance','nearest_fac')
     
     for i in range(num_steps):#for each timestep  
         print("step: {}".format(i))
